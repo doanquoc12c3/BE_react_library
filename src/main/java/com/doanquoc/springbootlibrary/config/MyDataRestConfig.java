27c5bc2067037e4de8bd7a89ea813478e8bd9b8b
unknown <doanquoc12c3>
2023-09-02 17:38:11 +0800

    add review repository

--- src/main/java/com/doanquoc/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/doanquoc/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.doanquoc.springbootlibrary.config;
 
 import com.doanquoc.springbootlibrary.entity.Book;
+import com.doanquoc.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -23,8 +24,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                                             HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
-
+        config.exposeIdsFor(Review.class);
         disableHttpMethods(Book.class,config,theUnsupportedActions );
+        disableHttpMethods(Review.class, config,theUnsupportedActions);
 
         cors.addMapping(config.getBasePath()+ "/**")
                 .allowedOrigins(theAllowedOrigins);
